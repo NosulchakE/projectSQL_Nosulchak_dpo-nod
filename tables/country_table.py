ceca699383ec7ffae6434c0e0c4483683094bce4
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2025-11-17 16:26:19 +0300

    Update country_table.py

--- tables/country_table.py
+++ tables/country_table.py
@@ -1,22 +1,31 @@
-# Таблица персоны и особые действия с ней
+from dbconnection import *
 
-from dbtable import *
+class CountriesTable:
+    dbconn = None
 
-class PeopleTable(DbTable):
     def table_name(self):
-        return self.dbconn.prefix + "people"
+        return self.dbconn.prefix + "countries"
 
-    def columns(self):
-        return {"id": ["serial", "PRIMARY KEY"],
-                "last_name": ["varchar(32)", "NOT NULL"],
-                "first_name": ["varchar(32)", "NOT NULL"],
-                "second_name": ["varchar(32)"]}
-
-    def find_by_position(self, num):
-        sql = "SELECT * FROM " + self.table_name()
-        sql += " ORDER BY "
-        sql += ", ".join(self.primary_key())
-        sql += " LIMIT 1 OFFSET %(offset)s"
+    def create(self):
         cur = self.dbconn.conn.cursor()
-        cur.execute(sql, {"offset": num - 1})
-        return cur.fetchone()       
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS countries (
+            country_name VARCHAR(100) PRIMARY KEY,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+        self.dbconn.conn.commit()
+
+    def all(self, limit=None, offset=None):
+        sql = "SELECT country_name FROM " + self.table_name() + " ORDER BY country_name"
+        if limit:
+            sql += f" LIMIT {limit} OFFSET {offset}"
+        cur = self.dbconn.conn.cursor()
+        cur.execute(sql)
+        return cur.fetchall()
+
+    def insert_one(self, name):
+        cur = self.dbconn.conn.cursor()
+        cur.execute("INSERT INTO countries(country_name) VALUES (%s)", (name,))
+        self.dbconn.conn.commit()
+
