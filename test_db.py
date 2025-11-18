c1dc03fcec145856d13d4058319190a5c06db051
Nosulchak Elena <nosulchak_elena@mail.ru>
2025-11-18 13:23:29 +0300

    final commit

--- test_db.py
+++
@@ -1,9 +0,0 @@
-from project_config import ProjectConfig
-from dbconnection import DbConnection
-
-cfg = ProjectConfig()
-try:
-    conn = DbConnection(cfg)
-    print("Соединение с PostgreSQL успешно!")
-except Exception as e:
-    print("Ошибка соединения:", e)
